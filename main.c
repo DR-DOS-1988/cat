edabf2206b54b12945f0b6e3edad35c0562a97da
Dr. Dos <eamonn.r.watkins@gmail.com>
2025-04-30 18:40:25 -0500

    Update main.c

--- main.c
+++ main.c
@@ -8,22 +8,14 @@ void dump_file(FILE *f) {
 
 int main(int argc, char **argv) {
 	FILE *f;
-	int i, e = 0;
+	int i;
 	for (i = 1; i < argc; i++) {
 		char *arg = argv[i];
 		if (!(f = fopen(arg, "r"))) {
 			fprintf(stderr, "cat: %s: ", arg);
 			perror("");
-			e = 1;
 			continue;
 		}
-		fclose(f);
-	}
-	if (e) {
-		return 1;
-	}
-	for (i = 1; i < argc; i++) {
-		f = fopen(argv[i], "r");
 		dump_file(f);
 		fclose(f);
 	}
