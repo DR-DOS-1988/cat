351a88de71f13ba4f1ebe2158476d86210718f19
Dr. Dos <eamonn.r.watkins@gmail.com>
2025-05-21 23:22:10 -0500

    Update main.c

--- main.c
+++ main.c
@@ -11,7 +11,7 @@ int main(int argc, char **argv) {
 	int i;
 	for (i = 1; i < argc; i++) {
 		char *arg = argv[i];
-		if (!(f = fopen(arg, "r"))) {
+		if (!(f = fopen(arg, "rb"))) {
 			fprintf(stderr, "cat: %s: ", arg);
 			perror("");
 			continue;
